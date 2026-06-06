f0dd6534b3878cadf819c083175606d0e0ab6962
Vaibhav Katke <vaibhav.katke@austeresystems.com>
2026-06-06 17:17:51 +0530

    removed the UI from server.js file

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const redisClient = redis.createClient({
   port: 6379
 });
 
-app.get('/', function(req, res) {
-    redisClient.get('numVisits', function(err, numVisits) {
-        numVisitsToDisplay = parseInt(numVisits) + 1;
-        if (isNaN(numVisitsToDisplay)) {
-            numVisitsToDisplay = 1;
-        }
-       res.send(os.hostname() +': Number of visits is: ' + numVisitsToDisplay);
-        numVisits++;
-        redisClient.set('numVisits', numVisits);
-    });
-});
+// app.get('/', function(req, res) {
+//     redisClient.get('numVisits', function(err, numVisits) {
+//         numVisitsToDisplay = parseInt(numVisits) + 1;
+//         if (isNaN(numVisitsToDisplay)) {
+//             numVisitsToDisplay = 1;
+//         }
+//        res.send(os.hostname() +': Number of visits is: ' + numVisitsToDisplay);
+//         numVisits++;
+//         redisClient.set('numVisits', numVisits);
+//     });
+// });
 
 app.listen(3000, function() {
     console.log('Web application is listening on port 3000');
