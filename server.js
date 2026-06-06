fa64ebf9b14b212d9402b55225e90a0672715053
Vaibhav Katke <vaibhav.katke@austeresystems.com>
2026-06-06 16:29:55 +0530

    Addedd new port for application

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('/', function(req, res) {
     });
 });
 
-app.listen(5000, function() {
-    console.log('Web application is listening on port 5000');
+app.listen(3000, function() {
+    console.log('Web application is listening on port 3000');
 });
